bbcaf3da1d622d8e493eb7e7b240aab635acd4b2
foolbread <981848153@qq.com>
2019-06-03 16:20:12 +0800

    +finish 35

---
+++ 35/35.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int idx = 0;
+        for (int i=0; i<nums.size(); i++)
+        {
+            if (nums[i] >= target)
+                break;
+            ++idx;
+        }
+
+        return idx;
+    }
+};
