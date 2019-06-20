9458450ec9c43155584a854f9cc404ab4801830c
foolbread <981848153@qq.com>
2019-06-20 15:19:46 +0800

    +finish 665…

---
+++ 665/665.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int asc = 0;
+        int des = 0;
+        int m = 0;
+        int n = nums.size()-1;
+
+        for (int i=0; i<nums.size(); i++)
+        {
+            if (nums[m] <= nums[i])
+                m = i;
+            else
+                ++asc;
+
+            if (nums[n] >= nums[nums.size()-1-i])
+                n = nums.size()-1-i;
+            else
+                ++des;
+            cout << asc << " " << des << endl;
+            if (asc > 1 && des > 1)
+                return false;
+        }
+
+        return true;
+    }
+};
