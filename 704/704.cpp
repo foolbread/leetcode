3a6be49dee206a07b4c37f4df736a36fbcba634a
foolbread <981848153@qq.com>
2019-09-23 14:15:26 +0800

    +finish 704…

---
+++ 704/704.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int right = nums.size()-1;
+        int left = 0;
+        int middle;
+
+        while (left <= right)
+        {
+            middle = (left+right)/2;
+            if (nums[middle] > target)
+            {
+                right = middle-1;
+            }
+            else if (nums[middle] < target)
+            {
+                left = middle+1;
+            }
+            else
+                return middle;
+        }
+
+        return -1;
+    }
+};
